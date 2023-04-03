1896b7a945ff880b49adb6aae5e2529c290ceae5
leonardobfn <75333445+leonardobfn@users.noreply.github.com>
2023-04-03 12:57:05 -0300

    Create rascunho_analise.R

---
+++ rascunho_analise.R
@@ -0,0 +1,22 @@
+require(tidyr)
+require(dplyr)
+dados = read.table("Data_simulation/Model_1/estimates/Method_2/estimates/estimatesalpha65")
+head(dados,10)
+conf = .05
+est = dados %>% group_by(V3) %>% summarise(m = median(V2,na.rm=T),
+                                     l = quantile(V2,conf/2,na.rm=T),
+                                     up = quantile(V2,1-(conf/2),na.rm=T))
+emv = read.table("Data_simulation/real_par_model_1.txt")
+
+
+
+id = dados$V1 %>% unique()%>%sort()
+id.f = seq(1,1000) %in% id
+length(which(id.f==F) )
+length(id)
+
+est = dados %>% group_by(V3) %>% summarise(m = median(V2,na.rm=F),
+                                           l = quantile(V2,conf/2,na.rm=F),
+                                           up = quantile(V2,1-(conf/2),na.rm=F))
+
+dados %>% slice(which(is.na(V2)==T))
